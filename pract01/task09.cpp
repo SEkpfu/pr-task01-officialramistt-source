9b039064b1357b7a4b0a32ef0a09ae2be696acb1
officialramistt-source <official.ramis.tt@gmail.com>
2025-10-06 11:14:45 +0300

    Add files via upload
    sec

--- pract01/task09.cpp
+++ pract01/task09.cpp
@@ -1,78 +1,131 @@
 #include <iostream>
 int summ(int, int, int);                                                                                        // получает на руку
-void db(int array_h[][2], int); 
-int tax(int, int, int);                                                 
+int cnt1(int, int, int);                                                                                        // без вычета налогов
+int cnt2(int);                                                                                                  // сумма налогов
+int db(int array_h[], int array_cash[], int);                                                                   // ввод данных работника
+int tax(int, int, int);                                                                                         // вычисление налога
+int sort(int array_total[], int count);                                                                         // сортировка
+int copyr(int array_total[], int sort_array_total[], int count);                                                // копия массива
+int find_numb(int array_total[], int sort_array_total[], int array_worke[], int count, int &n_max, int &n_min); // находит номер работника max и min и > 50000
+int findMinIndex(int arr[], int size);                                                                 
+int findMaxAndIndex(int arr[], int size, int& index);
+int countGreaterThan(int arr[], int size, int value);
+int sumArray(int arr[], int size);                                                                                                
 
-
-
-
-int main(){
-    setlocale(LC_ALL,"");
-    int array_inp[48][2];
-    int array_out[48][2];
-    int c_month;
-    int bonus = 13;
-    std::cout<<"Enter count month"<<std::endl;
-    std::cin>>c_month;
-    db(array_inp,c_month);
-    int i {0};
-    for (; i < 4; i++)
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int bonus = 10;
+    const int MAX_WORKER = 1000;
+    int count;
+    std::cout << "count of worker" << std::endl;
+    std::cin>>count;
+    int array_h[MAX_WORKER];
+    int array_cash[MAX_WORKER];
+    int array_total[MAX_WORKER];
+    int array_tax[MAX_WORKER];
+    int sort_array_total[MAX_WORKER];
+    int array_worke[MAX_WORKER]; 
+    int n_max{0};
+    int n_min{0};
+    db(array_h, array_cash, count);
+    for (int i{0}; i < count; i++)
     {
-        int st = c_month + c_month * i;
-        for(int r {i*c_month}; r<st;r++){
-            array_out[r][0] = summ(array_inp[r][0],array_inp[r][1], bonus);
-            array_out[r][1] = tax(array_inp[r][0],array_inp[r][1], bonus);
-        }
+        array_total[i] = summ(array_h[i], array_cash[i], bonus);
+        array_tax[i] = tax(array_h[i], array_cash[i], bonus);
     }
-    std::cout<<"1. =  "<< array_out[2][0]<<std::endl;
-    int summa {};
-    for (int m{0};m<c_month;m++){
-        summa+=array_out[m][1];
-    }
-    std::cout<<"2. =  "<<summa<<std::endl;
-    int mmax = -100;
-    int number;  
-    for (int mr{0}; mr<4; mr++){
-        if (mmax < array_out[ 2  + mr * c_month ][0]){
-            mmax = array_out[ 2  + mr * c_month ][0];
-            number = mr + 1;
-        }
-    }
-    std::cout<<"3. =  "<<number<<std::endl;
-    int summar = 0;
-    i = 0;
-    for (; i < 4; i++)
-    {
-        int st = c_month + c_month * i;
-        for(int r {i*c_month}; r<st;r++){
-            summar += array_out[r][0];
-        }
-    }
-    std::cout<<"4. =  "<<summar<<std::endl;
+    copyr(array_total, sort_array_total, count);
+    sort(sort_array_total, count);
+    
+    int minIndex = findMinIndex(array_total, count);
+    std::cout << "Номер минимального элемента: " << minIndex + 1 << std::endl;
+    
+    int maxValue;
+    int maxIndex;
+    maxValue = findMaxAndIndex(array_total, count, maxIndex);
+    std::cout << "Максимальный элемент: " << maxValue << " (номер: " << maxIndex + 1 << ")" << std::endl;
+    
+    
+    int countGreater = countGreaterThan(array_total, count, 50000);
+    std::cout << "Количество элементов > 50000: " << countGreater << std::endl;
+    
+   
+    int totalSum = sumArray(array_total, count);
+    std::cout << "Сумма всех элементов: " << totalSum << std::endl;
+    
+
+
+
+
+
+    return 0;
 }
+int findMinIndex(int arr[], int size) {
+    int minIndex = 0;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+
+int findMaxAndIndex(int arr[], int size, int& index) {
+    int maxValue = arr[0];
+    index = 0;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > maxValue) {
+            maxValue = arr[i];
+            index = i;
+        }
+    }
+    return maxValue;
+}
+
+int countGreaterThan(int arr[], int size, int value) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > value) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+
 int summ(int hour, int pay_hourly, int bonus)
 {
-    int pay = hour * pay_hourly + ((hour * pay_hourly) / 100) * bonus;
-    int tax = 13;
-    int tax_re = (pay / 100) * tax;
-    return pay - tax_re;
+    int pay = cnt1(hour, pay_hourly, bonus);
+    int res = cnt2(pay);
+    return pay - res;
 }
-void db(int array_h[][2], int count)
+int cnt1(int hour, int pay_hourly, int bonus)
 {
-    int i {0};
-    
-    for (; i < 4; i++)
+    int pay = hour * pay_hourly + ((hour * pay_hourly) / 100) * bonus;
+    return pay;
+}
+int cnt2(int pay)
+{
+    int tax = 13;
+    int res = (pay / 100) * tax;
+    return res;
+}
+int db(int array_h[], int array_cash[], int count)
+{
+    for (int i{0}; i < count; i++)
     {
-        int st = count + count * i;
-        std::cout<<"worker of "<<i+1<<std::endl;
-        int g = 0;
-        for(int r {i*count}; r<st;r++){
-            g++;
-            std::cout << "Enter hour/cash and hour/month "<< g <<" month "<< std::endl;
-            std::cin >> array_h[r][0] >> array_h[r][1];    
-        }
+        std::cout << "Enter hour and hour/cash " << i + 1<< std::endl;
+        std::cin >> array_h[i] >> array_cash[i];
     }
-    
+    return 0;
 }
 int tax(int hour, int pay_hourly, int bonus)
 {
@@ -80,4 +133,51 @@ int tax(int hour, int pay_hourly, int bonus)
     int tax = 13;
     int summ_tax = (total / 100) * tax;
     return summ_tax;
+}
+int sort(int array_total[], int count)
+{
+    for (int i{0}; i < count ; i++)
+    {
+        for (int j{0}; j < count - i - 1 ; j++)
+        {
+            if (array_total[j] > array_total[j+1])
+            {
+                int temp = array_total[j];
+                array_total[j] = array_total[j+1];
+                array_total[j+1] = temp;
+            }
+        }
+    }
+    return 0;
+}
+int copyr(int array_total[], int sort_array_total[], int count)
+{
+    for (int i{0}; i < count; i++)
+    {
+        sort_array_total[i] = array_total[i];
+    }
+    return  0;
+}
+int find_numb(int array_total[], int sort_array_total[], int array_worke[], int count, int &n_max, int &n_min)
+{
+    int maxx = sort_array_total[count - 1];
+    int minn = sort_array_total[0];
+    int g = 0;
+    for (int i{0}; i < count; i++)
+    {
+        if (maxx == array_total[i])
+        {
+            n_max = i + 1;
+        }
+        else if (minn == array_total[i])
+        {
+            n_min = i + 1;
+        }
+        if (array_total[i] > 50000)
+        {
+            array_worke[g] = i + 1;
+            g++;
+        }
+    }
+    return g;
 }
